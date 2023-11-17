b3385438ded8e639fe9388a5b4c9de53d3fcd52f
Maks6831 <ma.khan6971@gmail.com>
2023-11-17 13:43:47 +0000

    added table head

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Carousel } from './components/Carousel';
 import { CoinInfoContainer } from './components/CoinInfoContainer';
+import { Cointable } from './components/Cointable';
 import { Navbar } from './components/Navbar';
 import { Pricegraph } from './components/Pricegraph';
 import { Timebar } from './components/Timebar';
@@ -8,34 +9,34 @@ import "./globals.css";
 
 export default function Home() {
   return (
-   <div>
+  <div>
     <div>
       <Topbar/>
     </div>
     <div>
       <Navbar/>
     </div>
-      <div className=' bg-light-background w-full dark:bg-dark-background min-h-screen'>
-        <div className='mb-2'>
-          <Carousel/>
-        </div>
-        <div className='flex h-[25rem] w-full justify-center m-2'>
-          <div className=' m-2 p-6 bg-white-color rounded-xl  h-[25rem] w-[37rem] flex justify-center items-end relative dark:bg-light-text-color-two '>
-            <CoinInfoContainer isPrice={true}/>
-            <Pricegraph isLine={true}/>
-          </div>
-          <div className='m-2 p-6 bg-white-color rounded-xl  h-[25rem] w-[37rem] flex justify-center items-end relative dark:bg-volume-background'>
-            <CoinInfoContainer isPrice={false}/>
-            <Pricegraph isLine={false}/>
-          </div>
-        </div>
-        <div className='m-4 w-1/2 h-full flex justify-center'>
-            <Timebar/>
-        </div>
-      </div> 
-      <div>
-        
+    <div className=' bg-light-background w-full dark:bg-dark-background min-h-screen'>
+      <div className='mb-2'>
+        <Carousel/>
       </div>
-   </div>
+      <div className='flex h-[25rem] w-full justify-center m-2'>
+        <div className=' m-2 p-6 bg-white-color rounded-xl  h-[25rem] w-[37rem] flex justify-center items-end relative dark:bg-light-text-color-two '>
+          <CoinInfoContainer isPrice={true}/>
+          <Pricegraph isLine={true}/>
+        </div>
+        <div className='m-2 p-6 bg-white-color rounded-xl  h-[25rem] w-[37rem] flex justify-center items-end relative dark:bg-volume-background'>
+          <CoinInfoContainer isPrice={false}/>
+          <Pricegraph isLine={false}/>
+        </div>
+      </div>
+      <div className='m-4 w-1/2 h-full flex justify-center'>
+        <Timebar/>
+      </div>
+      <div className='flex justify-center items-center'>
+        <Cointable/>
+      </div>
+    </div> 
+  </div>
   )
 }
