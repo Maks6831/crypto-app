b7e36599636b19a7e7957fab8beee4d8ff0df965
Maks6831 <ma.khan6971@gmail.com>
2023-12-05 12:46:26 +0000

    implemented debouncing for search bar

--- src/app/components/Searchbar/index.tsx
+++ src/app/components/Searchbar/index.tsx
@@ -3,24 +3,27 @@ import React, { useEffect, useState } from 'react'
 import { searchData } from '@/app/GlobalRedux/Features/SearchData/searchSlice';
 import { useAppDispatch, useAppSelector } from '@/app/GlobalRedux/hooks';
 import { SearchItem } from '../SearchItem';
+import { useDebounce } from '@/app/Utils/useDebounce';
 
 export const Searchbar = () => {
   const dispatch = useAppDispatch();
   const { data, loading } = useAppSelector(state => state.searchReducer); 
   const [searchInput, setSearchInput] = useState('');
+  const [input, setInput] = useState('');
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => { 
+  const debouncedSearch = useDebounce(searchInput, 500);
+
+  const useHandleChange = (event: React.ChangeEvent<HTMLInputElement>) => { 
     setSearchInput(event.target.value);
   }
 
+  useEffect(()=>{
+      setInput(debouncedSearch);
+  },[debouncedSearch])
 
   useEffect(()=>{
     dispatch(searchData());
   },[])
-
-  useEffect(()=>{
-    console.log(loading)
-  },[searchInput])
 
   return (
     <div className='relative m-2 w-89'>
@@ -30,10 +33,10 @@ export const Searchbar = () => {
             </svg>
         </div>
         <label className='h-12 w-89 rounded-xl leading-10'>
-            <input  className='h-12 pl-8 w-89  bg-light-button-color bg-opacity-40 w-89 rounded-xl dark:bg-dark-button-color dark:bg-opacity-100' placeholder='Search...' onChange={handleChange}/>
+            <input  className='h-12 pl-8 w-89  bg-light-button-color bg-opacity-40 w-89 rounded-xl dark:bg-dark-button-color dark:bg-opacity-100' placeholder='Search...' type='text' value={searchInput} onChange={useHandleChange}/>
         </label>
         <div className='absolute left-0 top-0'>
-          {searchInput &&  
+          {input &&  
           data.filter(bit => bit.name.toLowerCase().includes(searchInput)).map((element)=> (
             <SearchItem
             key={element.id}
