4089e04132af8025b358fc5f25f095c8bdc7748e
Maks6831 <ma.khan6971@gmail.com>
2023-12-15 16:51:09 +0000

    removed console.logs

--- src/app/components/SearchItem/index.tsx
+++ src/app/components/SearchItem/index.tsx
@@ -3,15 +3,6 @@ import React, { RefObject, useEffect, useState } from 'react'
 export const SearchItem = ({name, index, changeIndex, opacity, keyPress}: {name: string, index: number, changeIndex: React.MouseEventHandler<HTMLDivElement>|Function, opacity: string, keyPress: boolean}) => {
   const [mouseOn, setMouseOn] = useState(false);
 
-  //const ToggleMouse = () => {
-  //  setMouseOn(!mouseOn);
-  //}
-
-  //const changeOpacity = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => { 
-  //  changeIndex(e)
-  //  ToggleMouse();
-  // console.log(index);
-  //}
   const nextOpacity = keyPress ? `${opacity} cursor-none` : 'hover:bg-opacity-90 bg-opacity-0 ';
 
   return (
