cff4681ffeb577f8b3a9ae6d70611cfc25a734a8
Maks6831 <ma.khan6971@gmail.com>
2023-12-17 20:40:30 +0000

    Buttonswitcher now removing and adding graphs and coin converter

--- src/app/components/Buttonswitcher/index.tsx
+++ src/app/components/Buttonswitcher/index.tsx
@@ -1,16 +1,10 @@
 'use client';
 import React, { useState } from 'react'
 
-export const Buttonswitcher = () => {
-  const [isCoin, setisCoin] = useState(true);
-
-  const setCoin = (value: boolean) => {
-    value ? setisCoin(true) : setisCoin(false);
-  }
-
-
+export const Buttonswitcher = ({setCoin, isCoin} : {setCoin: Function, isCoin: boolean}) => {
+  
   return (
-    <div className='mt-2 w-[31.6rem] h-14 rounded-md flex items-center bg-white dark:bg-dark-card  '>
+    <div className='mt-2 w-[31.6rem] h-14 rounded-md flex items-center bg-white dark:bg-dark-card cursor-pointer '>
         <div onClick={()=> setCoin(true)} className={isCoin ? 'w-full h-14  flex items-center justify-center bg-carousel-button-color-two bg-opacity-50 rounded-md': 'w-full h-14  flex items-center justify-center rounded-md'}>
           <div className='text-center'>Coins</div>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useState } from 'react';
 import { Buttonswitcher } from './components/Buttonswitcher';
 import { Carousel } from './components/Carousel';
 import { CoinInfoContainer } from './components/CoinInfoContainer';
@@ -7,31 +9,39 @@ import { Timebar } from './components/Timebar';
 import "./globals.css";
 
 export default function Home() {
+  const [isCoin, setisCoin] = useState(true);
+
+  const setCoin = (value: boolean) => {
+    value ? setisCoin(true) : setisCoin(false);
+  }
+
   return (
   <div>
     <div className=' bg-light-background w-full dark:bg-dark-background min-h-screen'>
-    <div className=' w-1/2 h-full flex justify-center'>
-      <Buttonswitcher/>
-    </div>
-      <div className='mb-2'>
-        <Carousel/>
+      <div className=' w-1/2 h-full flex justify-center'>
+        <Buttonswitcher setCoin={setCoin} isCoin={isCoin}/>
       </div>
-      <div className='flex h-[25rem] w-full justify-center m-2'>
-        <div className=' m-2 p-6 bg-white-color rounded-xl  h-[25rem] w-[37rem] flex justify-center items-end relative dark:bg-light-text-color-two '>
-          <CoinInfoContainer isPrice={true}/>
-          <Pricegraph isLine={true}/>
+      {isCoin ? 
+        <><div className='mb-2'>
+            <Carousel />
+          </div><div className='flex h-[25rem] w-full justify-center m-2'>
+              <div className=' m-2 p-6 bg-white-color rounded-xl  h-[25rem] w-[37rem] flex justify-center items-end relative dark:bg-light-text-color-two '>
+                <CoinInfoContainer isPrice={true} />
+                <Pricegraph isLine={true} />
+              </div>
+              <div className='m-2 p-6 bg-white-color rounded-xl  h-[25rem] w-[37rem] flex justify-center items-end relative dark:bg-volume-background'>
+                <CoinInfoContainer isPrice={false} />
+                <Pricegraph isLine={false} />
+              </div>
+            </div><div className='m-4 w-1/2 h-full flex justify-center'>
+              <Timebar />
+            </div></>
+        :
+        <div>Hello</div> 
+        }
+        <div className='flex justify-center items-center'>
+          <Cointable/>
         </div>
-        <div className='m-2 p-6 bg-white-color rounded-xl  h-[25rem] w-[37rem] flex justify-center items-end relative dark:bg-volume-background'>
-          <CoinInfoContainer isPrice={false}/>
-          <Pricegraph isLine={false}/>
-        </div>
-      </div>
-      <div className='m-4 w-1/2 h-full flex justify-center'>
-        <Timebar/>
-      </div>
-      <div className='flex justify-center items-center'>
-        <Cointable/>
-      </div>
     </div> 
   </div>
   )
