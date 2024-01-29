7bc2e21fcd592ff4cf7de6012dd23c8e23b78b08
Maks6831 <ma.khan6971@gmail.com>
2024-01-29 12:54:26 +0000

    Added skeleton coin page

--- src/app/coins/[coinId]/page.tsx
+++ src/app/coins/[coinId]/page.tsx
@@ -1,5 +1,16 @@
 'use client';
 
 export default function Page({ params }: { params: {coinId: string}}) {
-    return <div>My Post: {params.coinId}</div>
+    return <div style={{maxWidth: '1300px'}} className="border-2 h-screen flex p-1">
+      <div className="border-2 flex w-full p-3 ">
+        <div className="border-2 w-5/12 m-2 h-1/2">
+          <div>Card Component</div>
+        </div>
+        <div className="border-2 w-7/12 m-2 h-1/2 flex flex-col justify-between">
+          <div>Text</div>
+          <div>url container</div>
+        </div>
+      </div>
+      
+    </div>
   }
