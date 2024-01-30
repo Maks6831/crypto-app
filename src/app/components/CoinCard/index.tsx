0ba9d55fb6e170d0b65ad760deff7fde7c486b85
Maks6831 <ma.khan6971@gmail.com>
2024-01-30 21:47:56 +0000

    light mode for CoinCard

--- src/app/components/CoinCard/index.tsx
+++ src/app/components/CoinCard/index.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export const CoinCard = () => {
   return (
-    <div className=' w-11/12 h-full dark:bg-purplea rounded-xl flex flex-col p-2'>
+    <div className=' w-11/12 h-full  bg-carousel-button-color-two bg-opacity-40 dark:bg-purplea rounded-xl flex flex-col p-2'>
         <div className=' h-3/5 md:p-2  w-full '>
             <div className=' h-1/2 w-full flex flex-row-reverse md:flex-row md:justify-start justify-around  items-center    p-2'>
                 <div className=' h-20 w-20 md:h-12 md:w-12 '>Image</div>
                 <div className=' m-0 md:m-2'>
                     <div className='xl:text-xl lg:text-2xl md:text-lg sm:text-base text-sm font-bold '>Bitcoin (BTC)</div>
-                    <div className='font-medium xl:text-base lg:text-lg md:text-md sm:text-sm text-xs'>www.bitcoin.org</div>
+                    <div className='font-medium xl:text-base lg:text-lg md:text-md sm:text-sm text-xs text-light-text-color dark:text-card-text-gray'>www.bitcoin.org</div>
                 </div>
             </div>
             <div className=' h-1/2 w-full border-b border-opacity-25 border-card-text-gray flex flex-col justify-center p-2 md:p-0  '>
@@ -17,7 +17,7 @@ export const CoinCard = () => {
                     <div className='font-medium xl:text-xl lg:text-lg md:text-base sm:text-sm text-xs p-1'>^5.02%</div>
                 </div>
                 <div className='flex justify-start items-center'>
-                    <div className='xl:text-xl lg:text-lg md:text-base sm:text-sm text-xs p-1'>profit:</div>
+                    <div className='xl:text-xl lg:text-lg md:text-base sm:text-sm text-xs p-1 text-light-text-color dark:text-card-text-gray'>profit:</div>
                     <div className='xl:text-xl lg:text-lg md:text-base sm:text-sm text-xs p-1'>$1,504</div>
                 </div>
             </div>
@@ -27,16 +27,16 @@ export const CoinCard = () => {
             <div className=' h-1/2 w-full flex flex-col justify-center '>
                 <div className='flex justify-between w-full '>
                     <div className='xl:text-xl lg:text-lg md:text-base sm:text-sm text-xs'>All time high:</div>
-                    <div className='xl:text-2xl lg:text-xl md:text-lg sm:text-base text-sm'>$64,805</div>
+                    <div className='xl:text-2xl lg:text-xl md:text-lg sm:text-base text-sm text-purpleb dark:text-white'>$64,805</div>
                 </div>
-                <div className='text-card-text-gray xl:text-base lg:text-lg md:text-md sm:text-sm text-xs'>Wed, 14 Sep 2023 11:54:46 GMT</div>
+                <div className='text-light-text-color dark:text-card-text-gray xl:text-base lg:text-lg md:text-md sm:text-sm text-xs'>Wed, 14 Sep 2023 11:54:46 GMT</div>
             </div>
             <div className='h-1/2 w-full flex flex-col justify-center'>
             <div className='flex justify-between w-full '>
                     <div className='xl:text-xl lg:text-lg md:text-base sm:text-sm text-xs'>All time low:</div>
-                    <div className='xl:text-2xl lg:text-xl md:text-lg sm:text-base text-sm'>$64,805</div>
+                    <div className=' text-purpleb dark:text-white xl:text-2xl lg:text-xl md:text-lg sm:text-base text-sm'>$64,805</div>
                 </div>
-                <div className='text-card-text-gray xl:text-base lg:text-lg md:text-md sm:text-sm text-xs'>Wed, 14 Sep 2023 11:54:46 GMT</div>
+                <div className='text-light-text-color dark:text-card-text-gray xl:text-base lg:text-lg md:text-md sm:text-sm text-xs'>Wed, 14 Sep 2023 11:54:46 GMT</div>
             </div>  
         </div>
     </div>
