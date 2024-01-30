88d0ae33a95adcd9dcd3f7715e02a207ad2737fb
Maks6831 <ma.khan6971@gmail.com>
2024-01-30 14:53:01 +0000

    style DataCard Texts

--- src/app/components/DataCard/index.tsx
+++ src/app/components/DataCard/index.tsx
@@ -9,18 +9,18 @@ export const DataCard = ({data} : {data: string[][]}) => {
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                           </svg>
-                          <div className="p-2">{data[0][0]}</div>
+                          <div className="p-2  md:text-base sm:text-sm text-xs font-normal text-card-text-gray">{data[0][0]}</div>
                         </div>
-                        <div>{data[0][1]}</div>
+                        <div className='lg:text-xl md:text-lg sm:text-base text-sm'>{data[0][1]}</div>
                       </div>
                       <div className="flex justify-between w-full h-1/3   items-center p-5">
                         <div className="flex justify-center items-center">
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                           </svg>
-                          <div className="p-2">{data[1][0]}</div>
+                          <div className="p-2  md:text-base sm:text-sm text-xs font-normal text-card-text-gray">{data[1][0]}</div>
                         </div>
-                        <div>{data[1][1]}</div>
+                        <div className='lg:text-xl md:text-lg sm:text-base text-sm'>{data[1][1]}</div>
                       </div>
                       { data[2].length > 0 && 
                       <div className="flex justify-between w-full h-1/3 items-center p-5 ">
@@ -28,9 +28,9 @@ export const DataCard = ({data} : {data: string[][]}) => {
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                               <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                             </svg>
-                            <div className="p-2">{data[2][0]}</div>
+                            <div className="p-2  md:text-base sm:text-sm text-xs font-normal text-card-text-gray">{data[2][0]}</div>
                         </div>
-                        <div>{data[2][1]}</div>
+                        <div className='lg:text-xl md:text-lg sm:text-base text-sm'>{data[2][1]}</div>
                       </div>}
                     </div>
   )
