be5b17e28bca1388e4624217b96b3869595a1ce7
Maks6831 <ma.khan6971@gmail.com>
2024-01-31 13:44:42 +0000

    created asyncthunk

--- src/app/GlobalRedux/Features/CoinPage/coinPageSlice.ts
+++ src/app/GlobalRedux/Features/CoinPage/coinPageSlice.ts
@@ -1,3 +1,15 @@
 'use client';
 
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+
+export const coinPageData = createAsyncThunk(
+    'coinPageData',
+    async ({currency, amount}:
+      {currency: string, amount: string}, thunkApi) => {
+        const url = `https://api.coingecko.com/api/v3/coins/shiba-inu?localization=false&tickers=false&market_data=true&community_data=true&developer_data=false&sparkline=false&x_cg_demo_api_key=${process.env.NEXT_PUBLIC_API_KEY}`;
+        const response = await fetch(url);
+        const json = await response.json();
+        return json;
+      }
+    );
