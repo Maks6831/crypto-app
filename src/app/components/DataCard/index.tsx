8e563176adc2e820f70ec1f70e84401a66e74db9
Maks6831 <ma.khan6971@gmail.com>
2024-02-02 18:05:30 +0000

    styled plus button in data card

--- src/app/components/DataCard/index.tsx
+++ src/app/components/DataCard/index.tsx
@@ -16,9 +16,9 @@ export const DataCard = ({data, isProgress} : {data: (string | number)[][], isPr
     <div className=" shadow-lg bg-coin-card-color bg-opacity-30 dark:bg-purplea rounded-xl max-h-5/6 w-full">
                       <div className="flex flex-col min-[430px]:flex-row justify-between w-full h-1/3  items-center p-3 min-[430px]:p-5">
                         <div className="flex justify-center items-center">
-                          <div className=' rounded-full bg-carousel-button-color-one bg-opacity-20 border-carousel-button-color-two border-opacity-25 shadow-sm shadow-carousel-button-color-two dark:shadow-purpla'>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                              <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                          <div className=' rounded-full bg-opacity-70  bg-carousel-button-color-two p-1 border-light-text-color border-opacity-20 dark:border-carousel-button-color-two border-2 text-white shadow-sm dark:shadow-md shadow-coin-card-color dark:shadow-black    '>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className=" rounded-full bg-opacity-40  w-3 h-3 ">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                             </svg>
                           </div>
                           <div className="p-2  md:text-base sm:text-sm text-xs font-normal text-light-text-color dark:text-card-text-gray">{data[0][0]}</div>
@@ -27,9 +27,9 @@ export const DataCard = ({data, isProgress} : {data: (string | number)[][], isPr
                       </div>
                       <div className="flex flex-col min-[430px]:flex-row justify-between w-full h-1/3   items-center p-3 min-[430px]:p-5">
                         <div className="flex justify-center items-center">
-                          <div className=' rounded-full bg-carousel-button-color-one bg-opacity-20 border-carousel-button-color-two border-opacity-25 shadow-sm shadow-carousel-button-color-two dark:shadow-purpla'>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                              <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                          <div className=' rounded-full bg-opacity-70  bg-carousel-button-color-two p-1 border-light-text-color border-opacity-20 dark:border-carousel-button-color-two border-2 text-white shadow-sm dark:shadow-md shadow-coin-card-color dark:shadow-black    '>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className=" rounded-full bg-opacity-40  w-3 h-3 ">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                             </svg>
                           </div>
                           <div className="p-2  md:text-base sm:text-sm text-xs font-normal text-light-text-color dark:text-card-text-gray">{data[1][0]}</div>
@@ -56,9 +56,9 @@ export const DataCard = ({data, isProgress} : {data: (string | number)[][], isPr
                        data[2].length > 0 && 
                       <div className="flex flex-col min-[430px]:flex-row justify-between w-full h-1/3 items-center p-3 min-[430px]:p-5 ">
                         <div className="flex justify-center items-center">
-                          <div className=' rounded-full bg-carousel-button-color-one bg-opacity-20  border-opacity-25 shadow-sm shadow-carousel-button-color-two dark:shadow-coin-page-progress'>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                              <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                          <div className=' rounded-full bg-opacity-70  bg-carousel-button-color-two p-1 border-light-text-color border-opacity-20 dark:border-carousel-button-color-two border-2 text-white shadow-sm dark:shadow-md shadow-coin-card-color dark:shadow-black    '>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className=" rounded-full bg-opacity-40  w-3 h-3 ">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                             </svg>
                           </div>
                             <div className="p-2  md:text-base sm:text-sm text-xs font-normal text-light-text-color dark:text-card-text-gray">{data[2][0]}</div>
