62f39d4cbe5e90bcdc1a2f8c59a86b122e0f4acb
Maks6831 <ma.khan6971@gmail.com>
2024-02-21 23:38:54 +0000

    onClick on coin table now working

--- src/app/components/Cointable/index.tsx
+++ src/app/components/Cointable/index.tsx
@@ -1,65 +1,69 @@
-'use client';
-import { changeAmount, tableData } from '@/app/GlobalRedux/Features/Tabledata/TableSlice';
-import { useAppDispatch, useAppSelector } from '@/app/GlobalRedux/hooks'
-import React, { useEffect, useState } from 'react'
-import { TableElement } from '../TableElement';
-import { TableHeader } from '../TableHeader';
-import InfiniteScroll from 'react-infinite-scroll-component';
+"use client";
+import {
+  changeAmount,
+  tableData,
+} from "@/app/GlobalRedux/Features/Tabledata/TableSlice";
+import { useAppDispatch, useAppSelector } from "@/app/GlobalRedux/hooks";
+import React, { useEffect, useState } from "react";
+import { TableElement } from "../TableElement";
+import { TableHeader } from "../TableHeader";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export const Cointable = () => {
   const dispatch = useAppDispatch();
-  const { currency } = useAppSelector(state => state.currencyReducer);
-  const { coins, amount } = useAppSelector(state => state.tableReducer);
+  const { currency } = useAppSelector((state) => state.currencyReducer);
+  const { coins, amount } = useAppSelector((state) => state.tableReducer);
   const [dataLength, setDataLength] = useState(0);
 
   const onScroll = () => {
     const newAmount = +amount + 30;
     const amountStringified = newAmount.toString();
-    dispatch(tableData({currency, amount: amountStringified}));
+    dispatch(tableData({ currency, amount: amountStringified }));
     dispatch(changeAmount(amountStringified));
-  }
+  };
 
-  useEffect(()=>{
-    dispatch(tableData({currency, amount}));
-  },[])
+  useEffect(() => {
+    dispatch(tableData({ currency, amount }));
+  }, []);
 
-  useEffect(()=>{
-    setDataLength(coins.length)
-  },[coins])
-  
+  useEffect(() => {
+    setDataLength(coins.length);
+  }, [coins]);
+
   return (
-    <div className='flex justify-center items-center w-full '>
+    <div className="flex justify-center items-center w-full ">
       <InfiniteScroll
-            dataLength={dataLength}
-            next={onScroll}
-            hasMore={true}
-            loader={<h4>Loading...</h4>}
-          >   
-      <table className='w-full mx-auto border-separate border-spacing-y-3'>
-        <TableHeader/>
-        <tbody className='w-full'>
-          {coins &&
-          coins.map((coin, index) => (
-              <TableElement
-                key={coin.id}
-                number={coin.market_cap_rank}
-                name={coin.name}
-                image={coin.image}
-                symbol={coin.symbol}
-                price={coin.current_price}
-                change1h={coin.price_change_percentage_1h_in_currency}
-                change24h={coin.price_change_percentage_24h}
-                change7d={coin.price_change_percentage_7d_in_currency}
-                volume24h={coin.total_volume}
-                marketCap={coin.market_cap}
-                circulating={coin.circulating_supply}
-                totalSupply={coin.total_supply}
-                sparkline={coin.sparkline_in_7d.price}
-              />
-            ))}
-        </tbody>
-      </table>
+        dataLength={dataLength}
+        next={onScroll}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        <table className="w-full mx-auto border-separate border-spacing-y-3">
+          <TableHeader />
+          <tbody className="w-full">
+            {coins &&
+              coins.map((coin, index) => (
+                <TableElement
+                  key={coin.id}
+                  id={coin.id}
+                  number={coin.market_cap_rank}
+                  name={coin.name}
+                  image={coin.image}
+                  symbol={coin.symbol}
+                  price={coin.current_price}
+                  change1h={coin.price_change_percentage_1h_in_currency}
+                  change24h={coin.price_change_percentage_24h}
+                  change7d={coin.price_change_percentage_7d_in_currency}
+                  volume24h={coin.total_volume}
+                  marketCap={coin.market_cap}
+                  circulating={coin.circulating_supply}
+                  totalSupply={coin.total_supply}
+                  sparkline={coin.sparkline_in_7d.price}
+                />
+              ))}
+          </tbody>
+        </table>
       </InfiniteScroll>
     </div>
-  )
-}
+  );
+};
--- src/app/components/TableElement/index.tsx
+++ src/app/components/TableElement/index.tsx
@@ -1,67 +1,140 @@
-'use client';
-import React, { useEffect } from 'react';
-import Image from 'next/image';
-import { ProgressBar } from '../Progressbar';
-import { moneyConverter } from '@/app/Utils/moneyConverter';
-import { useAppSelector } from '@/app/GlobalRedux/hooks';
-import { TableBar } from '../TableBar';
-import { useTheme } from 'next-themes';
-import { Sparkline } from '../Sparkline';
-import { percentFormatter } from '@/app/Utils/percentFormatter';
-import { colorChange } from '@/app/Utils/colorChange';
+"use client";
+import React, { useEffect } from "react";
+import Image from "next/image";
+import { ProgressBar } from "../Progressbar";
+import { moneyConverter } from "@/app/Utils/moneyConverter";
+import { useAppSelector } from "@/app/GlobalRedux/hooks";
+import { TableBar } from "../TableBar";
+import { useTheme } from "next-themes";
+import { Sparkline } from "../Sparkline";
+import { percentFormatter } from "@/app/Utils/percentFormatter";
+import { colorChange } from "@/app/Utils/colorChange";
+import { useRouter } from "next/navigation";
 
-export const TableElement = ({number, name, image, symbol, price, change1h, change24h, change7d, sparkline, volume24h, marketCap, circulating, totalSupply} 
-  : 
-  {number:number, name: string, image: string, symbol:string, price:number, change1h:number, change24h:number, change7d:number, sparkline:number[], volume24h:number, marketCap:number, circulating:number, totalSupply:number}) => {
-    const { theme } = useTheme();
-    const reduxSymbol = useAppSelector(state => state.currencyReducer.symbol)
-    
+export const TableElement = ({
+  number,
+  name,
+  image,
+  symbol,
+  price,
+  change1h,
+  change24h,
+  change7d,
+  sparkline,
+  volume24h,
+  marketCap,
+  circulating,
+  totalSupply,
+  id,
+}: {
+  number: number;
+  name: string;
+  image: string;
+  symbol: string;
+  price: number;
+  change1h: number;
+  change24h: number;
+  change7d: number;
+  sparkline: number[];
+  volume24h: number;
+  marketCap: number;
+  circulating: number;
+  totalSupply: number;
+  id: string;
+}) => {
+  const { theme } = useTheme();
+  const reduxSymbol = useAppSelector((state) => state.currencyReducer.symbol);
+  const router = useRouter();
+
+  const goToPage = () => router.push(`coins/${id}`);
 
   return (
-    <tr  className=' h-20 mb-3  gap-4 cursor-pointer  w-full  first:rounded-l-xl '>
-        <td className=' bg-white dark:bg-dark-card  p-5 rounded-l-xl hidden xl:table-cell '><span className='flex flex-row items-center justify-center'>{number}</span></td>
-        <td className=' bg-white dark:bg-dark-card  h-full     rounded-l-xl'>
-          <div className='   w-36 md:w-full h-full flex justify-around sm:justify-start items-center '>
-            <div className='  m-2  '>
-               <Image
-                 src={image}
-                 alt={name}
-                 width={24}
-                 height={24}
-                 style={{
-                     width: '24px',
-                     height: '24px'
-                 }}
-               />
-             </div>
-            <div className='md:flex md:flex-row-reverse md:justify-end bg-white dark:bg-dark-card  h-full w-1/2 md:w-full '>
-              <div className=' text-sm md:text-base'>&nbsp;({symbol.toUpperCase()})</div>
-              <div className=' text-xs md:text-base'>{name}</div>
-            </div>
+    <tr
+      onClick={goToPage}
+      className=" h-20 mb-3  gap-4 cursor-pointer  w-full  first:rounded-l-xl "
+    >
+      <td className=" bg-white dark:bg-dark-card  p-5 rounded-l-xl hidden xl:table-cell ">
+        <span className="flex flex-row items-center justify-center">
+          {number}
+        </span>
+      </td>
+      <td className=" bg-white dark:bg-dark-card  h-full     rounded-l-xl">
+        <div className="   w-36 md:w-full h-full flex justify-around sm:justify-start items-center ">
+          <div className="  m-2  ">
+            <Image
+              src={image}
+              alt={name}
+              width={24}
+              height={24}
+              style={{
+                width: "24px",
+                height: "24px",
+              }}
+            />
           </div>
-        </td>
-        <td className=' bg-white dark:bg-dark-card md:p-5'><span className='flex flex-row items-center justify-center text-sm '>{reduxSymbol}{price.toFixed(2)}</span><span style={{color: `${colorChange(change7d, theme)}`}} className='flex md:hidden flex-row justify-center items-center text-xs'>{percentFormatter(change7d)}</span></td>
-        <td className=' bg-white dark:bg-dark-card   hidden xl:table-cell ' style={{color: `${colorChange(change1h, theme)}`}}><span className='flex flex-row justify-center items-center'>{percentFormatter(change1h)}</span></td>
-        <td className=' bg-white dark:bg-dark-card  hidden xl:table-cell' style={{color: `${colorChange(change24h, theme)}`}}><span className='flex flex-row justify-center items-center'>{percentFormatter(change24h)}</span></td>
-        <td className=' bg-white dark:bg-dark-card hidden md:table-cell  h-full  ' style={{color: `${colorChange(change7d, theme)}`}}><span className='flex flex-row justify-center items-center '>{percentFormatter(change7d)}</span></td>
-        <TableBar number={number}
-          numerator={volume24h}
-          denominator={marketCap}
-          change1h={change1h}
-          isFirst={true}
-        />
-        <TableBar
-          number={number}
-          numerator={circulating}
-          denominator={totalSupply}
-          change1h={change1h}
-          isFirst={false}
-        />
-        <Sparkline
+          <div className="md:flex md:flex-row-reverse md:justify-end bg-white dark:bg-dark-card  h-full w-1/2 md:w-full ">
+            <div className=" text-sm md:text-base">
+              &nbsp;({symbol.toUpperCase()})
+            </div>
+            <div className=" text-xs md:text-base">{name}</div>
+          </div>
+        </div>
+      </td>
+      <td className=" bg-white dark:bg-dark-card md:p-5">
+        <span className="flex flex-row items-center justify-center text-sm ">
+          {reduxSymbol}
+          {price.toFixed(2)}
+        </span>
+        <span
+          style={{ color: `${colorChange(change7d, theme)}` }}
+          className="flex md:hidden flex-row justify-center items-center text-xs"
+        >
+          {percentFormatter(change7d)}
+        </span>
+      </td>
+      <td
+        className=" bg-white dark:bg-dark-card   hidden xl:table-cell "
+        style={{ color: `${colorChange(change1h, theme)}` }}
+      >
+        <span className="flex flex-row justify-center items-center">
+          {percentFormatter(change1h)}
+        </span>
+      </td>
+      <td
+        className=" bg-white dark:bg-dark-card  hidden xl:table-cell"
+        style={{ color: `${colorChange(change24h, theme)}` }}
+      >
+        <span className="flex flex-row justify-center items-center">
+          {percentFormatter(change24h)}
+        </span>
+      </td>
+      <td
+        className=" bg-white dark:bg-dark-card hidden md:table-cell  h-full  "
+        style={{ color: `${colorChange(change7d, theme)}` }}
+      >
+        <span className="flex flex-row justify-center items-center ">
+          {percentFormatter(change7d)}
+        </span>
+      </td>
+      <TableBar
+        number={number}
+        numerator={volume24h}
+        denominator={marketCap}
+        change1h={change1h}
+        isFirst={true}
+      />
+      <TableBar
+        number={number}
+        numerator={circulating}
+        denominator={totalSupply}
+        change1h={change1h}
+        isFirst={false}
+      />
+      <Sparkline
         sparklineData={sparkline}
         change7d={change7d}
         number={number}
-        />
+      />
     </tr>
-  )
-}
+  );
+};
