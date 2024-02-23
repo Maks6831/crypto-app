c60107b153213f0f1aa49a4232c1e91cdcfb423d
Maks6831 <ma.khan6971@gmail.com>
2024-02-23 23:24:03 +0000

    Asset card now completed - just need json

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -20,13 +20,13 @@ export default function Portfolio() {
         </div>
         <div className="w-full min-h-32  flex justify-center ">
           <div className="w-11/12 bg-light-text-color-two h-72 p-3 flex">
-            <div className="w-2/12 flex justify-center items-center">
+            <div className="w-3/12 flex justify-center items-center">
               <div className="flex flex-col justify-center items-center">
                 <div>Image</div>
                 <div className="font-bold text-2xl">Name of coin</div>
               </div>
             </div>
-            <div className="w-10/12 p-2">
+            <div className="w-9/12 p-2">
               <div className="flex w-full h-1/2 flex-col">
                 <div className="flex justify-between">
                   <div className="font-medium text-xl">Market Price</div>
@@ -84,6 +84,57 @@ export default function Portfolio() {
                 </div>
               </div>
               <hr></hr>
+              <div className="flex w-full h-1/2 flex-col">
+                <div className="flex justify-between">
+                  <div className="font-medium text-xl">Your Coin</div>
+                  <div className="w-10 h-10 rounded-sm flex justify-center items-center dark:bg-edit-button-color">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <div className="w-full  h-1/2 flex justify-between">
+                  <div className="flex justify-center items-center flex-col">
+                    <div className=" text-sm font-normal dark:text-card-text-gray">
+                      Coin Amount
+                    </div>
+                    <div className="text-positive text-base">$39,504</div>
+                  </div>
+                  <div className="flex justify-center items-center flex-col">
+                    <div className=" text-sm font-normal dark:text-card-text-gray">
+                      Amount Value
+                    </div>
+                    <div className="text-positive text-base">
+                      {numberFormatter(45406, false, symbol)}
+                    </div>
+                  </div>
+                  <div className="flex justify-center items-center flex-col">
+                    <div className=" text-sm font-normal dark:text-card-text-gray">
+                      Acount Price Change Since Purchase
+                    </div>
+                    <div className="text-positive">
+                      {numberFormatter(35.32, true, "")}
+                    </div>
+                  </div>
+                  <div className="flex justify-center items-center flex-col">
+                    <div className=" text-sm font-normal dark:text-card-text-gray">
+                      Purchase Date
+                    </div>
+                    <div className="text-positive">3.2.2021</div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
