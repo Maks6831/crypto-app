0565a1196344bdf20f1812b5f9e264bdcd2533bf
Maks6831 <ma.khan6971@gmail.com>
2024-03-14 19:09:25 +0000

    added loading spinner to tablet

--- src/app/components/Cointable/index.tsx
+++ src/app/components/Cointable/index.tsx
@@ -8,11 +8,14 @@ import React, { useEffect, useState } from "react";
 import { TableElement } from "../TableElement";
 import { TableHeader } from "../TableHeader";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { LoadingSpinner } from "../LoadingSpinner";
 
 export const Cointable = () => {
   const dispatch = useAppDispatch();
   const { currency } = useAppSelector((state) => state.currencyReducer);
-  const { coins, amount } = useAppSelector((state) => state.tableReducer);
+  const { coins, amount, loading } = useAppSelector(
+    (state) => state.tableReducer
+  );
   const [dataLength, setDataLength] = useState(0);
 
   const onScroll = () => {
@@ -31,39 +34,50 @@ export const Cointable = () => {
   }, [coins]);
 
   return (
-    <div className="flex justify-center items-center w-full ">
-      <InfiniteScroll
-        dataLength={dataLength}
-        next={onScroll}
-        hasMore={true}
-        loader={<h4>Loading...</h4>}
-      >
-        <table className="w-full mx-auto border-separate border-spacing-y-3">
-          <TableHeader />
-          <tbody className="w-full">
-            {coins &&
-              coins.map((coin, index) => (
-                <TableElement
-                  key={coin.id}
-                  id={coin.id}
-                  number={coin.market_cap_rank}
-                  name={coin.name}
-                  image={coin.image}
-                  symbol={coin.symbol}
-                  price={coin.current_price}
-                  change1h={coin.price_change_percentage_1h_in_currency}
-                  change24h={coin.price_change_percentage_24h}
-                  change7d={coin.price_change_percentage_7d_in_currency}
-                  volume24h={coin.total_volume}
-                  marketCap={coin.market_cap}
-                  circulating={coin.circulating_supply}
-                  totalSupply={coin.total_supply}
-                  sparkline={coin.sparkline_in_7d.price}
-                />
-              ))}
-          </tbody>
-        </table>
-      </InfiniteScroll>
-    </div>
+    <>
+      {loading && coins.length === 0 && (
+        <div className="w-full h-20">
+          <div className="w-14 h-14">
+            <LoadingSpinner />
+          </div>
+        </div>
+      )}
+      {coins.length > 0 && (
+        <div className="flex justify-center items-center w-full ">
+          <InfiniteScroll
+            dataLength={dataLength}
+            next={onScroll}
+            hasMore={true}
+            loader={<h4>Loading...</h4>}
+          >
+            <table className="w-full mx-auto border-separate border-spacing-y-3">
+              <TableHeader />
+              <tbody className="w-full">
+                {coins &&
+                  coins.map((coin, index) => (
+                    <TableElement
+                      key={coin.id}
+                      id={coin.id}
+                      number={coin.market_cap_rank}
+                      name={coin.name}
+                      image={coin.image}
+                      symbol={coin.symbol}
+                      price={coin.current_price}
+                      change1h={coin.price_change_percentage_1h_in_currency}
+                      change24h={coin.price_change_percentage_24h}
+                      change7d={coin.price_change_percentage_7d_in_currency}
+                      volume24h={coin.total_volume}
+                      marketCap={coin.market_cap}
+                      circulating={coin.circulating_supply}
+                      totalSupply={coin.total_supply}
+                      sparkline={coin.sparkline_in_7d.price}
+                    />
+                  ))}
+              </tbody>
+            </table>
+          </InfiniteScroll>
+        </div>
+      )}
+    </>
   );
 };
