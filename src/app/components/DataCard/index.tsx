3b4933f51c2acdb5a492230d2ac424163fc5ee6d
Maks6831 <ma.khan6971@gmail.com>
2024-03-18 22:41:45 +0000

    added loading div to data card

--- src/app/components/DataCard/index.tsx
+++ src/app/components/DataCard/index.tsx
@@ -2,6 +2,8 @@
 import React, { useEffect } from "react";
 import { ProgressBar } from "../Progressbar";
 import { useTheme } from "next-themes";
+import { useAppSelector } from "@/app/GlobalRedux/hooks";
+import { LoadingSpinner } from "../LoadingSpinner";
 
 export const DataCard = ({
   data,
@@ -11,91 +13,21 @@ export const DataCard = ({
   isProgress: boolean;
 }) => {
   const { theme } = useTheme();
+  const { loading } = useAppSelector((state) => state.coinPageReducer);
 
   return (
     <div className=" dark:shadow-lg bg-white dark:bg-opacity-30 dark:bg-purplea rounded-xl max-h-5/6 w-full">
-      <div className="flex flex-col min-[430px]:flex-row justify-between w-full h-1/3  items-center p-3 min-[430px]:p-5">
-        <div className="flex justify-center items-center">
-          <div className=" rounded-full bg-opacity-30  dark:bg-opacity-70  bg-carousel-button-color-two p-1 border-light-text-color border-opacity-20 dark:border-carousel-button-color-two border-2 text-blue-700 dark:text-white  shadow-sm dark:shadow-md shadow-coin-card-color dark:shadow-black    ">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="2.5"
-              stroke="currentColor"
-              className=" rounded-full bg-opacity-40  w-3 h-3 "
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M12 4.5v15m7.5-7.5h-15"
-              />
-            </svg>
-          </div>
-          <div className="p-2  md:text-base sm:text-sm text-xs font-normal text-light-text-color dark:text-card-text-gray">
-            {data[0][0]}
-          </div>
-        </div>
-        <div className="lg:text-xl md:text-lg sm:text-base text-sm">
-          {data[0][1]}
-        </div>
-      </div>
-      <div className="flex flex-col min-[430px]:flex-row justify-between w-full h-1/3   items-center p-3 min-[430px]:p-5">
-        <div className="flex justify-center items-center">
-          <div className=" rounded-full bg-opacity-30  dark:bg-opacity-70  bg-carousel-button-color-two p-1 border-light-text-color border-opacity-20 dark:border-carousel-button-color-two border-2 text-blue-700 dark:text-white  shadow-sm dark:shadow-md shadow-coin-card-color dark:shadow-black    ">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="2.5"
-              stroke="currentColor"
-              className=" rounded-full bg-opacity-40  w-3 h-3 "
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M12 4.5v15m7.5-7.5h-15"
-              />
-            </svg>
-          </div>
-          <div className="p-2  md:text-base sm:text-sm text-xs font-normal text-light-text-color dark:text-card-text-gray">
-            {data[1][0]}
-          </div>
-        </div>
-        <div className="lg:text-xl md:text-lg sm:text-base text-sm">
-          {data[1][1]}
-        </div>
-      </div>
-      {isProgress && typeof data[2][1] === "number" ? (
-        <div>
-          <div className=" flex flex-col justify-between w-full items-center md:items-start max-h-1/3 start p-3  ">
-            <div className="flex items-center justify-between w-full ">
-              <div className="p-1   md:text-base sm:text-sm text-xs font-normal text-opacity-90 text-light-text-color dark:text-coin-page-progress">
-                Circulating/Max{" "}
-              </div>
-              <div className=" md:text-base sm:text-sm text-xs font-normal text-light-blue dark:text-coin-page-progress-two">
-                {data[2][1].toFixed(2)}%
-              </div>
-            </div>
-            {theme && (
-              <ProgressBar
-                percentage={+data[2][1]}
-                color={
-                  theme === "dark" ? "rgb(212,119,12)" : "rgb(99, 143, 254)"
-                }
-                size="min-w-full h-2"
-                backgroundColor={
-                  theme === "dark" ? "bg-coin-page-progress-two" : "bg-blue-100"
-                }
-              />
-            )}
+      {loading ? (
+        <div className="w-full h-full flex min-h-[10rem] justify-center items-center">
+          <div className="h-14 w-14">
+            <LoadingSpinner />
           </div>
         </div>
       ) : (
-        data[2].length > 0 && (
-          <div className="flex flex-col min-[430px]:flex-row justify-between w-full h-1/3 items-center p-3 min-[430px]:p-5 ">
+        <>
+          <div className="flex flex-col min-[430px]:flex-row justify-between w-full h-1/3  items-center p-3 min-[430px]:p-5">
             <div className="flex justify-center items-center">
-              <div className=" rounded-full  bg-opacity-30  dark:bg-opacity-70  bg-carousel-button-color-two p-1 border-light-text-color border-opacity-20 dark:border-carousel-button-color-two border-2 text-blue-700 dark:text-white shadow-sm dark:shadow-md shadow-coin-card-color dark:shadow-black    ">
+              <div className=" rounded-full bg-opacity-30  dark:bg-opacity-70  bg-carousel-button-color-two p-1 border-light-text-color border-opacity-20 dark:border-carousel-button-color-two border-2 text-blue-700 dark:text-white  shadow-sm dark:shadow-md shadow-coin-card-color dark:shadow-black    ">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -112,14 +44,97 @@ export const DataCard = ({
                 </svg>
               </div>
               <div className="p-2  md:text-base sm:text-sm text-xs font-normal text-light-text-color dark:text-card-text-gray">
-                {data[2][0]}
+                {data[0][0]}
               </div>
             </div>
             <div className="lg:text-xl md:text-lg sm:text-base text-sm">
-              {data[2][1]}
+              {data[0][1]}
             </div>
           </div>
-        )
+          <div className="flex flex-col min-[430px]:flex-row justify-between w-full h-1/3   items-center p-3 min-[430px]:p-5">
+            <div className="flex justify-center items-center">
+              <div className=" rounded-full bg-opacity-30  dark:bg-opacity-70  bg-carousel-button-color-two p-1 border-light-text-color border-opacity-20 dark:border-carousel-button-color-two border-2 text-blue-700 dark:text-white  shadow-sm dark:shadow-md shadow-coin-card-color dark:shadow-black    ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2.5"
+                  stroke="currentColor"
+                  className=" rounded-full bg-opacity-40  w-3 h-3 "
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 4.5v15m7.5-7.5h-15"
+                  />
+                </svg>
+              </div>
+              <div className="p-2  md:text-base sm:text-sm text-xs font-normal text-light-text-color dark:text-card-text-gray">
+                {data[1][0]}
+              </div>
+            </div>
+            <div className="lg:text-xl md:text-lg sm:text-base text-sm">
+              {data[1][1]}
+            </div>
+          </div>
+          {isProgress && typeof data[2][1] === "number" ? (
+            <div>
+              <div className=" flex flex-col justify-between w-full items-center md:items-start max-h-1/3 start p-3  ">
+                <div className="flex items-center justify-between w-full ">
+                  <div className="p-1   md:text-base sm:text-sm text-xs font-normal text-opacity-90 text-light-text-color dark:text-coin-page-progress">
+                    Circulating/Max{" "}
+                  </div>
+                  <div className=" md:text-base sm:text-sm text-xs font-normal text-light-blue dark:text-coin-page-progress-two">
+                    {data[2][1].toFixed(2)}%
+                  </div>
+                </div>
+                {theme && (
+                  <ProgressBar
+                    percentage={+data[2][1]}
+                    color={
+                      theme === "dark" ? "rgb(212,119,12)" : "rgb(99, 143, 254)"
+                    }
+                    size="min-w-full h-2"
+                    backgroundColor={
+                      theme === "dark"
+                        ? "bg-coin-page-progress-two"
+                        : "bg-blue-100"
+                    }
+                  />
+                )}
+              </div>
+            </div>
+          ) : (
+            data[2].length > 0 && (
+              <div className="flex flex-col min-[430px]:flex-row justify-between w-full h-1/3 items-center p-3 min-[430px]:p-5 ">
+                <div className="flex justify-center items-center">
+                  <div className=" rounded-full  bg-opacity-30  dark:bg-opacity-70  bg-carousel-button-color-two p-1 border-light-text-color border-opacity-20 dark:border-carousel-button-color-two border-2 text-blue-700 dark:text-white shadow-sm dark:shadow-md shadow-coin-card-color dark:shadow-black    ">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="2.5"
+                      stroke="currentColor"
+                      className=" rounded-full bg-opacity-40  w-3 h-3 "
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M12 4.5v15m7.5-7.5h-15"
+                      />
+                    </svg>
+                  </div>
+                  <div className="p-2  md:text-base sm:text-sm text-xs font-normal text-light-text-color dark:text-card-text-gray">
+                    {data[2][0]}
+                  </div>
+                </div>
+                <div className="lg:text-xl md:text-lg sm:text-base text-sm">
+                  {data[2][1]}
+                </div>
+              </div>
+            )
+          )}
+        </>
       )}
     </div>
   );
